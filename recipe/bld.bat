ed018bb3781c71f111c21b74da6629ae7b4377c8
Eric Lundby <Eric.Lundby@gmail.com>
2026-09-11 13:02:12 -0600

    PKG-16685: fix pre-existing build rot - disable Doxygen find (docs not required)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,7 @@ mkdir build && cd build
 cmake -G"%CMAKE_GENERATOR%" ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=ON ^
   -DMKLROOT="%LIBRARY_PREFIX%" ^
   %SRC_DIR%
 if errorlevel 1 exit 1
